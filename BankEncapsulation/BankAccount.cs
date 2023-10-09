6d41e819edeeb450190ab3e69b06759ce9a3ec90
James Yarbrough <jjarbrough@gmail.com>
2023-10-09 14:04:46 -0500

    finished the exercise with a little style

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double _balance = 0;
+
+        public BankAccount()
+        {
+        }
+        public BankAccount(double balance)
+        {
+            _balance = balance;
+        }
+
+        public void Deposit(double addMoney)
+        {
+            _balance += addMoney;
+        }
+        public void Withdraw(double subtractMoney)
+        {
+            _balance -= subtractMoney;
+        }
+
+        public double GetBalance() 
+        {
+            Console.WriteLine($"Your bank account has ${_balance}");
+            return _balance;
+        }
+
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount jamesAcc = new BankAccount(500);
+            bool finished = false;
+            while (!finished)
+            {
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("type 1 for check balance");
+                Console.WriteLine("type 2 for deposit");
+                Console.WriteLine("type 3 for withdrawal");
+                Console.WriteLine("type 4 for finished");
+                int option = int.Parse(Console.ReadLine());
+                if (option == 1)
+                {
+                    jamesAcc.GetBalance();
+                }
+                else if (option == 2)
+                {
+                    Console.WriteLine("how much would you like to deposit?");
+                    int add = int.Parse(Console.ReadLine());
+                    jamesAcc.Deposit(add);
+                }
+                else if (option == 3)
+                {
+                    Console.WriteLine("how much would you like to withdraw?");
+                    int subtract = int.Parse(Console.ReadLine());
+                    jamesAcc.Withdraw(subtract);
+                }
+                else if (option == 4)
+                {
+                    Console.WriteLine("bye");
+                    finished = true;
+                }
+            }
         }
     }
 }
